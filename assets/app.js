1da032bedac11d8d2a2d3ccb1cfe0f2cc510817a
itsmegh <42684917+itsmegh@users.noreply.github.com>
2018-10-22 17:37:29 -0600

    finished layout for html and starting code for javascript

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,34 @@
 $(document).ready(function(){
+//SETUP VARIABLES
+//==========================
+  var APIKey = "59944c64d39b4d578acf50b00ab62ffd";
 
-    var APIKey = "59944c64d39b4d578acf50b00ab62ffd";
+  //search parameters
+  var queryTerm = "";
+  var numResults = 0;
+  var startYear = 0;
+  var endYear = 0;
+
+        
+  var queryurlBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + APIKey ;
+
+//FUNCTIONS
+// =========================
+
+//MAIN PROCESSES
+// =========================
+
+// 1. Retrieve user inputs and convert to variables
+// 2. Use variables to run an AJAX call to NYT
+// 3. Break down the NYT object into usable fields
+// 4. Dynamically generate html content
+
+// 5. Deal with "edge cases" -- bugs that aren't obvious or didn't expect
+
+
+
 
     
-        
-    var queryurl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + APIKey ;
     
     $.ajax({
       url: queryurl,
