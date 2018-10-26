cb39bf5c245ace3d72a2c6fd55018e04f74edd69
itsmegh <42684917+itsmegh@users.noreply.github.com>
2018-10-26 12:47:53 -0600

    Adding in javascript, updated page design

--- assets/app.js
+++ assets/app.js
@@ -12,12 +12,61 @@ $(document).ready(function(){
         
   var queryurlBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + APIKey ;
 
+  //variable to track the number of articles
+  var articleCounter = 0;
+
 //FUNCTIONS
 // =========================
 
+  function runQuery(numArticles, queryurl){
+    //AJAX function
+    $.ajax({url: queryurl, method: "GET"})
+    .done(function(NYTdata) {
+
+      //log to the console so we have access to the information we need
+      console.log(queryurl);
+      console.log(NYTdata);
+    })
+
+  }
+
+  
+
 //MAIN PROCESSES
 // =========================
+  $("#searchBtn").on("click", function() {
+    
+    queryTerm = $("#search").val().trim();
+    console.log(queryTerm);
 
+    //add in the search term
+    var newURL = queryurlBase + "&q=" + queryTerm;
+    console.log(newURL);
+
+    //get the number of records
+
+    //get the start and end year
+    startYear = $("#startYear").val().trim();
+    endYear = $("#endYear").val().trim();
+
+    //add the data information to the URL, and add in the month and date since only year required
+    if(parseInt(startYear)) {
+      startYear = startYear + "0101";
+      newURL = newURL + "&begin_date=" + startYear;
+    }
+
+    if(parseInt(endYear)) {
+      endYear = endYear + "0101";
+      newURL = newURL + "&end_date=" + endYear;
+    }
+    console.log(newURL);
+
+    //send the AJAX call the new assembled URL
+    runQuery(10, newURL);
+
+    return false; //prevents a new page refresh
+
+  })
 // 1. Retrieve user inputs and convert to variables
 // 2. Use variables to run an AJAX call to NYT
 // 3. Break down the NYT object into usable fields
